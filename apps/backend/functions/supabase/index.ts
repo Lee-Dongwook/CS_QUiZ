e38bf61076bae7ad8f95755535f450cefc46ceaf
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-02-02 16:40:22 +0900

    feat: connect supabase to server

---
+++ apps/backend/functions/supabase/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable object-curly-spacing */
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.SUPA_URL!,
+  process.env.SUPA_ANON_KEY!
+);
