913c9280a66efa8c9c41a5031deb55199f5e6ad6
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-02-02 15:54:47 +0900

    feat : supabase url and anon key

--- packages/app/utils/supabase.ts
+++ packages/app/utils/supabase.ts
@@ -3,8 +3,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import "react-native-url-polyfill/auto";
 import { AppState } from "react-native";
 
-const supabaseUrl = "";
-const supabaseAnonKey = "";
+const supabaseUrl = "https://jfolgcdqxnppspozihov.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impmb2xnY2RxeG5wcHNwb3ppaG92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0NzkyNTIsImV4cCI6MjA1NDA1NTI1Mn0.qNHS589-uN0CjPmjxwP2YPQWx-kNUZHuLWJjyTng4DI";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
